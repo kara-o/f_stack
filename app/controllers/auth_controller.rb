a30ea92d8b1e4b8a823f7f1f7877ad1447e82cd0
Kara Odle <karajo.odle@gmail.coml>
2019-10-01 16:07:23 -0700

    updated layouts, added current_user and logged_in, links for navbar

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,29 +1,23 @@
 class AuthController < ApplicationController
 
   def login
+    render layout: 'login'
   end
 
   def verify
-    byebug
     @user = User.find_by(username: params[:username])
-    session[:user_id] = @user.id
     if @user && @user.email == params[:email] #&& @user.authenticate(params[:password])
+      session[:user_id] = @user.id
       redirect_to @user
     else
       flash[:message] = "Invalid login credentials, please try again or create a new account!"
-      render :login
+      redirect_to login_path
     end
   end
 
   def logout
     session.clear
     redirect_to login_path
-  end
-
-  def current_user
-    if logged_in?
-      User.find(session[:user_id])
-    end
   end
 
 end
