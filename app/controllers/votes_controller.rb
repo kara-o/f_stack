6ffa200f12f7579000185369b519faf83d7d6744
Kara Odle <karajo.odle@gmail.coml>
2019-10-02 10:34:22 -0700

    about to add vote_count column to answers

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,10 +2,12 @@ class VotesController < ApplicationController
 
   def create
     @vote = Vote.new(answer_id: params[:vote][:answer_id], user_id: current_user.id)
-    if !@vote.save
+    if @vote.save
+      @vote.vote_count += 1
+    else
       flash[:message] = "You already voted for this answer!"
     end
-    redirect_to question_path(Answer.find(params[:vote][:answer_id]).question.id)
+    redirect_to question_path(@vote.answer.question.id)
   end
 
 end
