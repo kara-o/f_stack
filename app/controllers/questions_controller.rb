f8bc65378fcf4378643789de0e9f9edc4add1d03
Kara Odle <karajo.odle@gmail.coml>
2019-10-01 17:39:57 -0700

    writing out question show page, about to try nested forms

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,3 +1,37 @@
 class QuestionsController < ApplicationController
 
+  before_action :set_question, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @questions = Question.order(created_at: :desc)
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_question
+    @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:title, :content, :user_id)
+  end
+
 end
