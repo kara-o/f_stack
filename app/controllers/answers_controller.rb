17de79d009ee7b7e3e9b06966c3ff2420a97e92d
kara-o <44376276+kara-o@users.noreply.github.com>
2019-10-02 15:47:12 -0700

    finished delete status action

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,5 @@
 class AnswersController < ApplicationController
+  before_action :set_answer, only: [:show, :delete_status]
 
   def create
     @answer = Answer.create(content: params[:answer][:content], user_id: current_user.id, question_id: params[:answer][:question_id])
@@ -6,13 +7,18 @@ class AnswersController < ApplicationController
   end
 
   def show
-    @answer = Answer.find(params[:id])
     redirect_to question_path(@answer.question.id)
   end
 
   def delete_status
-
+    @answer.update(:content => "This post no longer exists", :deleted => true)
     redirect_to user_path(current_user.id)
+  end
+
+  private
+
+  def set_answer
+    @answer = Answer.find(params[:id])
   end
 
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :delete_status]
 
   def create
     Comment.create(content: params[:comment][:content], user_id: current_user.id, commentable_id: params[:comment][:commentable_id], commentable_type: params[:comment][:commentable_type])
@@ -6,7 +7,6 @@ class CommentsController < ApplicationController
   end
 
   def show
-    @comment = Comment.find(params[:id])
     if @comment.commentable_type == "Answer"
       redirect_to question_path(Answer.find(@comment.commentable_id).question_id)
     else
@@ -15,8 +15,14 @@ class CommentsController < ApplicationController
   end
 
   def delete_status
-
+    @comment.update(:content => "This post no longer exists", :deleted => true)
     redirect_to user_path(current_user.id)
+  end
+
+  private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
   end
 
 end
