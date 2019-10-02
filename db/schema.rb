bdfd383aaec0d0999e2f15433b39f20215c1c039
kara-o <44376276+kara-o@users.noreply.github.com>
2019-10-02 10:23:46 -0700

    added votes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_163927) do
+ActiveRecord::Schema.define(version: 2019_10_02_163257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,13 @@ ActiveRecord::Schema.define(version: 2019_10_01_163927) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "answer_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
